edf6b5da2acd83b4d3c3dc1499d5591a958cad20
EduEH <eduardo_hirai@hotmail.com>
2025-11-04 15:44:28 -0800

    Fix variable format

--- terraform/modules/web-server/variables.tf
+++ terraform/modules/web-server/variables.tf
@@ -1,6 +1,9 @@
 variable "project_name" {type = string}
 variable "ami" {type = string}
-variable "instance_type" {type = string; default = "t2.micro"}
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+}
 variable "key_name" {type = string}
 variable "vpc_security_group_ids" {type = list(string)}
 variable "subnet_id" {type = string}
